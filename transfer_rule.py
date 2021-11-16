3de9ceddd3c5bca9e7621de78349d5a8f320c4a3
karolbanasik <banasik.karol@yahoo.com>
2021-11-16 22:08:28 +0100

    transfer rules for king and face-up

--- transfer_rule.py
+++ transfer_rule.py
@@ -9,7 +9,8 @@ class TransferRule:
         self.object_from = object_from
         self.move_valid = True
         self.bottom_transferred_card = self.object_from.cards[-self.number_of_cards]
-        self.top_row_to_card = self.row_to.cards[-1]
+        if len(self.row_to.cards)> 0:
+            self.top_row_to_card = self.row_to.cards[-1]
 
     def check(self):
         if len(self.row_to.cards) > 0:
@@ -25,14 +26,14 @@ class TransferRule:
     def younger_on_older(self):
         bottom_transferred_index = CardRank.ranks.index(self.bottom_transferred_card.rank)
         top_row_index = CardRank.ranks.index(self.top_row_to_card.rank)
-        is_younger_on_older = top_row_index  == bottom_transferred_index + 1
+        is_younger_on_older = top_row_index == bottom_transferred_index + 1
         self.validness_trap_switch(is_younger_on_older)
 
     def king_on_empty(self):
-        pass
+        self.validness_trap_switch(self.bottom_transferred_card.rank == 'K')
 
     def transfer_to_face_up(self):
-        pass
+        self.validness_trap_switch(self.top_row_to_card.face_up)
 
     def validness_trap_switch(self, check_result):
         if self.move_valid:
@@ -40,11 +41,12 @@ class TransferRule:
 
 if __name__ == '__main__':
     card_a = Card(suit= 'hearts', rank='K')
-    card_b = Card(suit= 'spades', rank='Q')
+    card_b = Card(suit= 'spades', rank='K')
     from row import Row
     row_a = Row()
     row_b = Row()
-    row_a.cards.append(card_a)
+    #row_a.cards.append(card_a)
     row_b.cards.append(card_b)
-    TransferRule(row_b, row_a, 1).younger_on_older()
+    rule = TransferRule(row_b, row_a, 1)
+    rule.check()
     pass
